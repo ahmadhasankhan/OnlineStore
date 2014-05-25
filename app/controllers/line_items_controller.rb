51e01f711ea029163e784a03ec22c28d59a362fe
Ahmad Hasan Khan <er.ahmad.hassan@gmail.com>
2014-05-25 13:32:26 +0530

    Smart Cart added Quantity for item and rescue from invatid card Id

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,8 +27,7 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
-
+    @line_item = @cart.add_product(product.id)
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart,
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140525060042) do
+ActiveRecord::Schema.define(version: 20140525074255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140525060042) do
     t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity",   default: 1
   end
 
   add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id", using: :btree
