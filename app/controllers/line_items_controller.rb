551b6bddd5645980b5995abd2fa68c57661d9e36
Ahmad Hasan Khan <er.ahmad.hassan@gmail.com>
2014-05-25 14:54:18 +0530

    Shifter Cart To Side Bar

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,7 +30,7 @@ class LineItemsController < ApplicationController
     @line_item = @cart.add_product(product.id)
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_url }
         format.json { render action: 'show',
                              status: :created, location: @line_item }
       else
