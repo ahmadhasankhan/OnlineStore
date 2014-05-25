74e1a3607584e37ad2f38ecfc7f258970994d4fd
Ahmad Hasan Khan <er.ahmad.hassan@gmail.com>
2014-05-25 14:10:36 +0530

    Calculating total price

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,8 +30,7 @@ class LineItemsController < ApplicationController
     @line_item = @cart.add_product(product.id)
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart,
-                                  notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
         format.json { render action: 'show',
                              status: :created, location: @line_item }
       else
