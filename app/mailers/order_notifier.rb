0c77d4d489ad9c1edd4a4fc86a5305226f3ab75a
Ahmad Hasan Khan <er.ahmad.hassan@gmail.com>
2014-05-26 20:09:59 +0530

    Configured Mail Notification

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,23 @@
+class OrderNotifier < ActionMailer::Base
+  default from: "OnlineStore <asystemtraining@gmail.com>"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received(order)
+    @order = order
+    mail to: order.email, subject: 'Online Store Order Confirmation'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+    mail to: order.email, subject: 'Online Store Order Shipped'
+  end
+end
